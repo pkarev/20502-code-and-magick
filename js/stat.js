da5a1b2fe8925d86624fbd11c432c82aa3eb5ae1
Pavel Karev <pkaref@yandex.ru>
2018-09-03 23:06:49 +0300

    Refactoring renderStatistics function structure

--- js/stat.js
+++ js/stat.js
@@ -21,51 +21,40 @@ var LINE_HEIGHT = 20;
 window.renderStatistics = function (ctx, names, times) {
 
   var maxTime = Math.max.apply(0, times);
-
-  renderRect(CLOUD_SHAPE_X, CLOUD_SHAPE_Y, CLOUD_WIDTH, CLOUD_HEIGHT, 'rgba(0, 0, 0, 0.7)');
-  renderRect(CLOUD_START_X, CLOUD_START_Y, CLOUD_WIDTH, CLOUD_HEIGHT, 'black');
+  renderStatsCloud();
 
   var renderX = CLOUD_CENTER_X;
   var renderY = CLOUD_START_Y + 20;
 
-  ctx.font = FONT;
   ctx.fillStyle = 'white';
+  ctx.font = FONT;
   ctx.textAlign = 'center';
-
   renderCongrats(renderX, renderY, CONGRATS);
 
   renderX = CLOUD_START_X + (CLOUD_WIDTH - BAR_WIDTH * 4 - BAR_GAP * 3) / 2 + BAR_WIDTH / 2;
 
   times.forEach(function (item, index) {
+    var barHeight = item * BAR_MAX_HEIGHT / maxTime;
     var renderYShift = (1 - item / maxTime) * BAR_MAX_HEIGHT;
     renderY = CLOUD_START_Y + LINE_HEIGHT * CONGRATS.length + LINE_HEIGHT * 2 + renderYShift;
-    var timeLegend = Math.floor(item);
 
-    ctx.fillStyle = 'white';
-    ctx.fillText(timeLegend, renderX, renderY);
+    renderTime(item);
 
-    var playerTime = item;
-    var playerName = names[index];
-    var barColor = BAR_COLOR;
-    var barHeight = playerTime * BAR_MAX_HEIGHT / maxTime;
-    if (playerName !== 'Вы') {
-      barColor = generateRandomRGB();
-    }
     renderX -= BAR_WIDTH / 2;
     renderY += LINE_HEIGHT / 2;
-    renderRect(renderX, renderY, BAR_WIDTH, barHeight, barColor);
+
+    renderBar(names[index], barHeight);
 
     renderX += BAR_WIDTH / 2;
     renderY += LINE_HEIGHT + barHeight;
-    ctx.fillStyle = 'white';
-    ctx.fillText(playerName, renderX, renderY);
 
+    renderName(names[index]);
     renderX += BAR_WIDTH + BAR_GAP;
   });
 
-  function renderRect(startX, startY, width, height, color) {
-    ctx.fillStyle = color;
-    ctx.fillRect(startX, startY, width, height);
+  function renderStatsCloud() {
+    renderRect(CLOUD_SHAPE_X, CLOUD_SHAPE_Y, CLOUD_WIDTH, CLOUD_HEIGHT, 'rgba(0, 0, 0, 0.7)');
+    renderRect(CLOUD_START_X, CLOUD_START_Y, CLOUD_WIDTH, CLOUD_HEIGHT, 'black');
   }
 
   function renderCongrats(x, y, texts) {
@@ -75,6 +64,26 @@ window.renderStatistics = function (ctx, names, times) {
       }
       ctx.fillText(item, x, renderY);
     });
+  }
+
+  function renderTime(time) {
+    var timeLegend = Math.floor(time);
+    ctx.fillText(timeLegend, renderX, renderY);
+  }
+
+  function renderBar(name, barHeight) {
+    var barColor = name === 'Вы' ? BAR_COLOR : generateRandomRGB();
+    renderRect(renderX, renderY, BAR_WIDTH, barHeight, barColor);
+  }
+
+  function renderName(name) {
+    ctx.fillStyle = 'white';
+    ctx.fillText(name, renderX, renderY);
+  }
+
+  function renderRect(startX, startY, width, height, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(startX, startY, width, height);
   }
 
   function generateRandomRGB() {
