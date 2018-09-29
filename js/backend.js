50d561bff72e9934c0513104d2afc96535b115cc
Karev <pavel.karev@pegast.ru>
2018-09-29 23:00:23 +0300

    Module 6 task 1
    Load similar wizards from server
    Send setup form data via ajax and close it after send

---
+++ js/backend.js
@@ -0,0 +1,67 @@
+'use strict';
+
+(function () {
+  var URLSend = 'https://js.dump.academy/code-and-magick';
+  var URLGet = 'https://js.dump.academy/code-and-magick/data';
+
+
+  window.backend = {
+    load: function (onLoad, onError) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+      xhr.timeout = 1;
+      xhr.open('GET', URLGet);
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          onLoad(xhr.response);
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за ' + xhr.timeout / 1000 + ' секунд');
+      });
+
+      xhr.send();
+    },
+
+    save: function (data, onLoad, onError) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          if (xhr.status === 200) {
+            onLoad(xhr.response);
+          } else {
+            onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+          }
+        }
+      });
+
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+
+      xhr.open('POST', URLSend);
+      xhr.send(data);
+    },
+
+    error: function (errorMessage) {
+      var node = document.createElement('div');
+      node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+      node.style.position = 'absolute';
+      node.style.left = 0;
+      node.style.right = 0;
+      node.style.fontSize = '30px';
+
+      node.textContent = errorMessage;
+      document.body.insertAdjacentElement('afterbegin', node);
+    }
+  };
+})();
--- js/setup.js
+++ js/setup.js
@@ -1,51 +1,32 @@
 'use strict';
 
 (function () {
-  var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-  var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+  var SIMILAR_WIZARDS_NUMBER = 4;
   var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
   var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
   var wizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-  var wizards = new Array(4);
   var wizardsFragment = document.createDocumentFragment();
 
-  generateWizards(wizards);
-  renderSimilarWizards();
-  document.querySelector('.setup-similar').classList.remove('hidden');
-
-  function getFullName(names, surnames) {
-    return names[window.util.getRandomIndex(names.length)] + ' ' + surnames[window.util.getRandomIndex(surnames.length)];
-  }
 
   function getColor(colors) {
     return colors[window.util.getRandomIndex(colors.length)];
   }
 
-  function generateWizards(array) {
-    for (var i = 0; i < array.length; i++) {
-      var wizard = {};
-      wizard.name = getFullName(NAMES, SURNAMES);
-      wizard.coatColor = getColor(COAT_COLORS);
-      wizard.eyesColor = getColor(EYES_COLORS);
-      array[i] = wizard;
-    }
-  }
-
   function createWizardElement(wizardParams) {
     var wizardElement = wizardTemplate.cloneNode(true);
     wizardElement.querySelector('.setup-similar-label').textContent = wizardParams.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizardParams.coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizardParams.eyesColor;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizardParams.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizardParams.colorEyes;
 
     return wizardElement;
   }
 
-  function renderSimilarWizards() {
-    wizards.forEach(function (similarWizard) {
-      wizardsFragment.appendChild(createWizardElement(similarWizard));
-    });
+  function renderSimilarWizards(wizardsArray) {
+    for (var i = 0; i <= SIMILAR_WIZARDS_NUMBER - 1; i++) {
+      wizardsFragment.appendChild(createWizardElement(wizardsArray[i]));
+    }
 
     document.querySelector('.setup-similar-list').appendChild(wizardsFragment);
   }
@@ -74,4 +55,17 @@
     evt.target.style.backgroundColor = newColor;
     wizardFireBallInput.setAttribute('value', newColor);
   });
+
+  var form = document.querySelector('.setup-wizard-form');
+
+  form.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+
+    window.backend.save(new FormData(form), function () {
+      window.closeSetup();
+    });
+  });
+
+  window.backend.load(renderSimilarWizards, window.backend.error);
+  document.querySelector('.setup-similar').classList.remove('hidden');
 })();
